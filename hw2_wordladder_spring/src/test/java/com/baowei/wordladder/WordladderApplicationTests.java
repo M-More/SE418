11e77c0af611452a09ae0999670e497a2125c27e
M-More <710599679@qq.com>
2019-04-07 21:31:02 +0800

    add test

--- hw2_wordladder_spring/src/test/java/com/baowei/wordladder/WordladderApplicationTests.java
+++ hw2_wordladder_spring/src/test/java/com/baowei/wordladder/WordladderApplicationTests.java
@@ -1,16 +1,39 @@
 package com.baowei.wordladder;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class WordladderApplicationTests {
+    @Autowired
+    private WebApplicationContext wac;
 
+    private MockMvc mockMvc;
+
+    @Before
+    public void setup(){
+        mockMvc = MockMvcBuilders.webAppContextSetup(wac).build();
+    }
+    
     @Test
-    public void contextLoads() {
+    public void whenQuerySuccess () throws Exception{
+        mockMvc.perform(MockMvcRequestBuilders.get("/showladder")
+                .param("word1","cat")
+                .param("word2","dog")
+                .contentType(MediaType.APPLICATION_JSON_UTF8))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.length()").value("4"));
     }
 
 }
